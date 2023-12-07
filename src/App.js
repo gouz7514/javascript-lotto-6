c11c7588985c2d2910fd8edb496bd7d3004ecde0
gouz7514 <gouz7514@gmail.com>
2023-12-07 14:45:40 +0900

    Feat : 기능 1-2. 당첨 번호를 입력 받는다

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import OutputView from "./View/OutputView.js";
 class App {
   async play() {
     const money = await this.#getMoney();
+    const luckyNumbers = await this.#getLuckyNumbers();
   }
 
   // 1-1. 로또 구입 금액을 입력받는다.
   async #getMoney() {
     try {
       const money = InputValidator.validateMoney(await InputView.getMoney());
-      return money;
+      return Number(money);
     } catch (error) {
       OutputView.printError(error.message);
       return this.#getMoney();
+    }
+  }
+
+  // 1-2. 당첨 번호를 입력 받는다.
+  async #getLuckyNumbers() {
+    try {
+      const luckyNumbers = InputValidator.validateLuckyNumbers(await InputView.getLuckyNumbers());
+      return luckyNumbers;
+    } catch (error) {
+      OutputView.printError(error.message);
+      return this.#getLuckyNumbers();
     }
   }
 }
--- src/Validator/InputValidator.js
+++ src/Validator/InputValidator.js
@@ -4,14 +4,48 @@ const InputValidator = {
     this.isDivideByUnit(value);
     return value;
   },
+  validateLuckyNumbers(value) {
+    const luckyNumbers = value.split(",").map((v) => Number(v));
+    this.validateArray(luckyNumbers, this.isNumber);
+    this.isLuckyLength(luckyNumbers);
+    this.validateArray(luckyNumbers, this.isLuckyRange);
+    this.isRepeat(luckyNumbers);
+    return luckyNumbers;
+  },
+  validateArray(array, callback) {
+    array.forEach((value) => {
+      callback(value);
+    });
+  },
+  // 숫자인지 검증
   isNumber(value) {
     if (Number.isNaN(value)) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
   },
+  // 1,000원 단위인지 검증
   isDivideByUnit(value) {
-    if (value % 1000 !== 0) {
+    if (Number(value) % 1000 !== 0) {
       throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+    }
+  },
+  // 6개의 숫자인지 검증
+  isLuckyLength(luckyNumbers) {
+    if (luckyNumbers.length !== 6) {
+      throw new Error("[ERROR] 6개의 숫자를 입력해주세요.");
+    }
+  },
+  // 1 ~ 45 사이의 숫자인지 검증
+  isLuckyRange(value) {
+    if (value < 1 || value > 45) {
+      throw new Error("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.");
+    }
+  },
+  // 중복된 숫자가 있는지 검증
+  isRepeat(luckyNumbers) {
+    const set = new Set(luckyNumbers);
+    if (set.size !== 6) {
+      throw new Error("[ERROR] 중복된 숫자가 있습니다.");
     }
   }
 };
